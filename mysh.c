1b77b1b2d265ad9fc203b0d4b50287722402a5f9
BENJIVN <bengi3288@gmail.com>
2024-11-14 13:55:26 -0500

    mysh> not prompting

--- mysh.c
+++ mysh.c
@@ -12,19 +12,18 @@
 void batch_mode(const char *filename);
 void interactive_mode();
 int parse_and_exec();
-void wildcard();
-int exec_command();
-int exec_pip_command();
+
 
 int main(int argc, char *argv[]){
     if (argc == 2){
         //printf("Entering batch mode with file: %s\n", argv[1]);
         batch_mode(argv[1]);
-    } else {
+    } else if (isatty(STDIN_FILENO)){
         //printf("Entering interactive mode\n");
         interactive_mode();
+    } else {
+        return 1;
     }
-
     return 0;
 }
 
@@ -47,22 +46,46 @@ void batch_mode(const char *filename){
 
 void interactive_mode(){
     //printf("Interactive mode not fully implemented yet.\n");
-    char command[MAX_LENTH];
+    char command[MAX_LENGTH];
     int status = 0;
+    int bytesRead = 0;
+    int index = 0;
 
     printf("Welcome to my shell! \n");
+
     while(1){
         printf("mysh> ");
-        if(!fgets(command, MAX_LENGTH, stdin)){
+        index = 0;
+         //printf("Waiting for input...\n");
+        while ((bytesRead = read(STDIN_FILENO, &command[index], 1)) > 0){
+            if (command[index] == '\n'){
+                command[index] = '\0'; //null-terminate the character
+                break;
+            }
+            index++;
+            if (index >= MAX_LENGTH - 1){
+                fprintf(stderr, "Error: command is too long\n");
+                index = 0; //reset the index 
+                break;
+            }
+        }
+        if (bytesRead <= 0){
             break;
         }
-        if(strcmp(command, "\n") == 0) {
-            continue
+        if (strcmp(command, "exit") == 0){
+            printf("exiting\n");
+            break;
         }
+        //fat fingering enter
+        if (strcmp(command, "") == 0){
+            continue;
+        }
+        status = parse_and_exec(command, status);
     }
 }
 
-int parse_and_exec();
-void wildcard();
-int exec_command();
-int exec_pip_command();
+int parse_and_exec(char *command, int status){
+    printf("Executing command: %s with status: %d\n", command, status);
+    return 0;
+}
+
